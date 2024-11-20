0f87eb7b614d7d54cadddf8d40e5c43c0bec677c
Isaí Arellano <isai.arellano@outlook.com>
2024-11-20 14:36:59 -0600

    Initial Commit

---
+++ Architecture_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture_MVVM
+//
+//  Created by Isaí Arellano on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
